d5c52c746ce817ea5319ca4ed7abd928d06dc2c2
MKhan1792 <kutubkhanmakda1792@gmail.com>
2023-01-10 17:55:18 +0530

    day-2

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
+import { Box } from '@mui/system';
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import ExerciseDetail from './Components/ExerciseDetail';
+import Home from './Components/Home';
+import Navbar from './Components/Navbar';
 
 function App() {
   return (
-    <div className="App">
-      App
-    </div>
+    <Box width='400px'>
+      <Navbar/>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/exercise/:id' element={<ExerciseDetail/>}/>
+      </Routes>
+    </Box>
   );
 }
 
