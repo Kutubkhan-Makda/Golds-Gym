d44901e710fe83582d2db66e2913a8c4fc60cbdb
MKhan1792 <kutubkhanmakda1792@gmail.com>
2023-01-11 16:07:58 +0530

    day-3

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import Footer from './Components/Footer';
 
 function App() {
   return (
-    <Box width='400px'>
+    <Box width='400px' sx={{width:{xl:'1488px'}}} m='auto'>
       <Navbar/>
       <Routes>
         <Route path='/' element={<Home/>}/>
