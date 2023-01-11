d6256c9d8fc0267a2cd119aa6399948858d4ef63
MKhan1792 <kutubkhanmakda1792@gmail.com>
2023-01-11 16:17:25 +0530

    day-3

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
     <Box width='400px' sx={{width:{xl:'1488px'}}} m='auto'>
       <Navbar/>
       <Routes>
-        <Route path='/' element={<Home/>}/>
+        <Route path='/Golds-Gym' element={<Home/>}/>
         <Route path='/exercise/:id' element={<ExerciseDetail/>}/>
       </Routes>
       <Footer/>
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,8 +1,15 @@
+import { Stack } from '@mui/system'
 import React from 'react'
+import { Link } from 'react-router-dom'
+import logo from '../assets/images/Logo.png'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <Stack>
+      <Link to='/Golds-Gym'>
+        <img src={logo} alt="logo" style={{width:'48px',height:'48px',margin:'0 20px'}} />
+      </Link>
+    </Stack>
   )
 }
 
