a72b9cc231b18a1a3302caf9ec69e26c330ce587
MKhan1792 <kutubkhanmakda1792@gmail.com>
2023-01-11 18:50:45 +0530

    day-3

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -5,11 +5,11 @@ import logo from '../assets/images/Logo.png'
 
 const Navbar = () => {
   return (
-    <Stack>
+    <Stack direction='row' justifyContent='space-around' sx={{gap:{sm:'112px',xs:'40px'},mt:{sm:'32px',xs:'20px'},justifyContent:'none'}} px='20px'>
       <Link to='/Golds-Gym'>
         <img src={logo} alt="logo" style={{width:'48px',height:'48px',margin:'0 20px'}} />
       </Link>
-      <Stack direction='row' gap='40px'>
+      <Stack direction='row' gap='40px' fontSize='24px' alignItems='flex-end'>
         <Link to='/Golds-Gym' style={{textDecoration:'none',color:'#3A1212',borderBottom:'3px solid #FF2625'}}>Home</Link>
         <a href="#exercises" style={{textDecoration:'none',color:'#3A1212'}}>Exersise</a>
       </Stack>
