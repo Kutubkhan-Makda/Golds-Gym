8def3c3cb99426aa26e65563127070183c7d6e57
MKhan1792 <kutubkhanmakda1792@gmail.com>
2023-01-12 01:01:10 +0530

    day-4

--- src/Components/HeroBanner.jsx
+++ src/Components/HeroBanner.jsx
@@ -1,6 +1,7 @@
 import { Button, Typography } from '@mui/material'
 import { Box } from '@mui/system'
 import React from 'react'
+import heroBannerImage from '../assets/images/banner.png'
 
 const HeroBanner = () => {
   return (
@@ -11,10 +12,11 @@ const HeroBanner = () => {
         <Typography fontWeight={700} sx={{fontSize:{lg:'44px',xs:'40px'}}}>
             Sweat, Smile <br /> and Repeat
         </Typography>
-        <Typography fontSize='22px' lineHeight='35px' mb={20}>
+        <Typography fontSize='22px' lineHeight='35px' mb={3}>
             Check out the most effective exercises 
         </Typography>
-        <Button variant='contained' color='error'>Explore Exercises</Button>
+        <Button variant='contained' color='error' href='#exercises'>Explore Exercises</Button>
+        <img src={heroBannerImage} alt="" />
     </Box>
   )
 }
