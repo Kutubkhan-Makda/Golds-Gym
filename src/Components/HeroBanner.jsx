2f74a17f9303b0b54cd94e508cb95f3eeea3af3e
MKhan1792 <kutubkhanmakda1792@gmail.com>
2023-01-11 19:17:08 +0530

    day-3

---
+++ src/Components/HeroBanner.jsx
@@ -0,0 +1,15 @@
+import { Typography } from '@mui/material'
+import { Box } from '@mui/system'
+import React from 'react'
+
+const HeroBanner = () => {
+  return (
+    <Box>
+        <Typography color='#FF2625' fontWeight='600'>
+            Fitness Club
+        </Typography>
+    </Box>
+  )
+}
+
+export default HeroBanner
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,16 @@
+import { Box } from '@mui/system'
 import React from 'react'
+import Exercises from '../Components/Exercises'
+import HeroBanner from '../Components/HeroBanner'
+import SearchExercises from '../Components/SearchExercises'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <Box>
+      <HeroBanner/>
+      <SearchExercises/>
+      <Exercises/>
+    </Box>
   )
 }
 
