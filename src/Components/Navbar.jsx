90f6fc9b6c6f76bedbc5ea5666be65722be580f0
MKhan1792 <kutubkhanmakda1792@gmail.com>
2023-01-11 16:24:08 +0530

    day-3

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -9,6 +9,10 @@ const Navbar = () => {
       <Link to='/Golds-Gym'>
         <img src={logo} alt="logo" style={{width:'48px',height:'48px',margin:'0 20px'}} />
       </Link>
+      <Stack direction='row' gap='40px'>
+        <Link to='/Golds-Gym' style={{textDecoration:'none',color:'#3A1212',borderBottom:'3px solid #FF2625'}}>Home</Link>
+        <a href="#exercises" style={{textDecoration:'none',color:'#3A1212'}}>Exersise</a>
+      </Stack>
     </Stack>
   )
 }
