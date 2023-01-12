425c20f04dbaa5607686fc7276aa02f7bcaa47ba
MKhan1792 <kutubkhanmakda1792@gmail.com>
2023-01-12 11:14:03 +0530

    day-4

--- src/Components/HeroBanner.jsx
+++ src/Components/HeroBanner.jsx
@@ -9,14 +9,17 @@ const HeroBanner = () => {
         <Typography color='#FF2625' fontWeight='600' fontSize='26px'>
             Fitness Club
         </Typography>
-        <Typography fontWeight={700} sx={{fontSize:{lg:'44px',xs:'40px'}}}>
+        <Typography fontWeight={700} mb='23px' mt='30px' sx={{fontSize:{lg:'44px',xs:'40px'}}}>
             Sweat, Smile <br /> and Repeat
         </Typography>
-        <Typography fontSize='22px' lineHeight='35px' mb={3}>
+        <Typography fontSize='22px' lineHeight='35px' mb={4}>
             Check out the most effective exercises 
         </Typography>
-        <Button variant='contained' color='error' href='#exercises'>Explore Exercises</Button>
-        <img src={heroBannerImage} alt="" />
+        <Button variant='contained' color='error' href='#exercises' sx={{backgroundColor:'#FF2625',padding:'10px'}}>Explore Exercises</Button>
+        <Typography fontWeight={600} color='#FF2625' fontSize='200px' sx={{opacity:0.1,display:{lg:'block',xs:'none'}}}>
+          Exercise
+        </Typography>
+        <img src={heroBannerImage} alt="banner" className='hero-banner-img'/>
     </Box>
   )
 }
