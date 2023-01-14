66857a2514c22a76350b21950d6d8a984359e6c7
MKhan1792 <kutubkhanmakda1792@gmail.com>
2023-01-15 00:51:42 +0530

    day-5

--- src/Components/SearchExercises.jsx
+++ src/Components/SearchExercises.jsx
@@ -3,8 +3,8 @@ import React from 'react'
 
 const SearchExercises = () => {
   return (
-    <Stack alignItems='center'>
-
+    <Stack alignItems='center' mt='37px' justifyContent='center' p='20px'>
+ 
     </Stack>
   )
 }
