1edf0bf8eb805519a1bad73d567093adeb76f02d
MKhan1792 <kutubkhanmakda1792@gmail.com>
2023-01-15 00:55:09 +0530

    day-5

--- src/Components/SearchExercises.jsx
+++ src/Components/SearchExercises.jsx
@@ -1,10 +1,13 @@
-import { Stack } from '@mui/material'
+import { Stack, Typography } from '@mui/material'
 import React from 'react'
 
 const SearchExercises = () => {
   return (
     <Stack alignItems='center' mt='37px' justifyContent='center' p='20px'>
- 
+      <Typography>
+        Awesome Exercises You <br />
+        Should Know
+      </Typography>
     </Stack>
   )
 }
