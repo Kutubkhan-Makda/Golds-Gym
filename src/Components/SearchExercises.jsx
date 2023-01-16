3fff1340bd6e18fe7b97838da721c1dc19c6113f
MKhan1792 <kutubkhanmakda1792@gmail.com>
2023-01-17 00:13:14 +0530

    day-7

--- src/Components/SearchExercises.jsx
+++ src/Components/SearchExercises.jsx
@@ -1,8 +1,10 @@
 import { Button, Stack, TextField, Typography } from '@mui/material'
 import { Box } from '@mui/system'
-import React from 'react'
+import React, { useState } from 'react'
 
 const SearchExercises = () => {
+  const [search, setSearch] = useState()
+
   return (
     <Stack alignItems='center' mt='37px' justifyContent='center' p='20px'>
       <Typography fontWeight={700} sx={{fontSize:{lg:'44px',xs:'30px'}}} md='50px' textAlign='center'>
@@ -10,8 +12,8 @@ const SearchExercises = () => {
         Should Know
       </Typography>
       <Box position='relative' md='72px'>
-        <TextField height='76px' value='' onChange={(e)=>{}} placeholder='Search Exercises' type='text' sx={{input:{fontWeight:'700',border:'none',borderRadius:'4px'},width:{lg:'1170px',xs:'350px'},backgroundColor:'#fff',borderRadius:'40px'}}/>
-        <Button className='search-btn' sx={{bgcolor:'#FF2625',color:'#fff',textTransform:'none',width:{lg:'175px',xs:'80px'}}}>Search</Button>
+        <TextField height='76px' value='' onChange={(e)=>{}} placeholder='Search Exercises' type='text' sx={{input:{fontWeight:'700',border:'none',borderRadius:'4px'},width:{lg:'800px',xs:'350px'},backgroundColor:'#fff',borderRadius:'40px'}}/>
+        <Button className='search-btn' sx={{bgcolor:'#FF2625',color:'#fff',textTransform:'none',width:{lg:'175px',xs:'80px'},fontSize:{lg:'20px',xs:'14px'},height:'56px',position:'absolute',right:'0'}}>Search</Button>
       </Box>
     </Stack>
   )
