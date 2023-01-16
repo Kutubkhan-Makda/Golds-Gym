ad05a0858d0bb8afaee4edb1e95eade98ffb4f36
MKhan1792 <kutubkhanmakda1792@gmail.com>
2023-01-16 22:53:30 +0530

    day-6

--- src/Components/SearchExercises.jsx
+++ src/Components/SearchExercises.jsx
@@ -4,7 +4,7 @@ import React from 'react'
 const SearchExercises = () => {
   return (
     <Stack alignItems='center' mt='37px' justifyContent='center' p='20px'>
-      <Typography>
+      <Typography fontWeight={700} sx={{fontSize:{lg:'44px',xs:'30px'}}} md='50px' textAlign='center'>
         Awesome Exercises You <br />
         Should Know
       </Typography>
