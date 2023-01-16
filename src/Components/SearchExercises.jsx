dd37407830f58ecc72ed5ca41349ee68eb7d785b
MKhan1792 <kutubkhanmakda1792@gmail.com>
2023-01-17 00:23:45 +0530

    day-7

--- src/Components/SearchExercises.jsx
+++ src/Components/SearchExercises.jsx
@@ -3,7 +3,13 @@ import { Box } from '@mui/system'
 import React, { useState } from 'react'
 
 const SearchExercises = () => {
-  const [search, setSearch] = useState()
+  const [search, setSearch] = useState('')
+
+  const handleSearch = async ()=>{
+    if(search){
+      
+    }
+  }
 
   return (
     <Stack alignItems='center' mt='37px' justifyContent='center' p='20px'>
@@ -12,8 +18,8 @@ const SearchExercises = () => {
         Should Know
       </Typography>
       <Box position='relative' md='72px'>
-        <TextField height='76px' value='' onChange={(e)=>{}} placeholder='Search Exercises' type='text' sx={{input:{fontWeight:'700',border:'none',borderRadius:'4px'},width:{lg:'800px',xs:'350px'},backgroundColor:'#fff',borderRadius:'40px'}}/>
-        <Button className='search-btn' sx={{bgcolor:'#FF2625',color:'#fff',textTransform:'none',width:{lg:'175px',xs:'80px'},fontSize:{lg:'20px',xs:'14px'},height:'56px',position:'absolute',right:'0'}}>Search</Button>
+        <TextField height='76px' value={search} onChange={(e)=>setSearch(e.target.value.toLowerCase())} placeholder='Search Exercises' type='text' sx={{input:{fontWeight:'700',border:'none',borderRadius:'4px'},width:{lg:'800px',xs:'350px'},backgroundColor:'#fff',borderRadius:'40px'}}/>
+        <Button className='search-btn' onClick={handleSearch} sx={{bgcolor:'#FF2625',color:'#fff',textTransform:'none',width:{lg:'175px',xs:'80px'},fontSize:{lg:'20px',xs:'14px'},height:'56px',position:'absolute',right:'0'}}>Search</Button>
       </Box>
     </Stack>
   )
