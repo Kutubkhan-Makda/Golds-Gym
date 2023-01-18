4a991458399ceafb64c53281147a04a5ad60bf56
MKhan1792 <kutubkhanmakda1792@gmail.com>
2023-01-18 16:24:42 +0530

    day-8

--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -1,3 +1,12 @@
+export const exerciseOption = {
+    method: 'GET',
+    url: 'https://exercisedb.p.rapidapi.com/exercises/bodyPartList',
+    headers: {
+      'X-RapidAPI-Key': process.env.REACT_RAPID_API_KEY,
+      'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com'
+    }
+  };
+
 export const fetchData = async (url,options)=>{
     const responce = await fetch(url,options);
     const data = await responce.json();
