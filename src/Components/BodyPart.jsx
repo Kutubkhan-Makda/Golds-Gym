e66dd41f49056664ce11f49a3ff3aec4ed4c50d3
MKhan1792 <kutubkhanmakda1792@gmail.com>
2023-01-21 21:35:29 +0530

    day-11

--- src/Components/BodyPart.jsx
+++ src/Components/BodyPart.jsx
@@ -1,11 +1,13 @@
+import { Typography } from '@mui/material'
 import { Stack } from '@mui/system'
 import React from 'react'
 import Icon from '../assets/icons/gym.png'
 
 const BodyPart = ({item,setBodyPart,BodyPart}) => {
   return (
-    <Stack type='button' alignItems='center' justifyContent='center' className='bodyPart-card' sx={BodyPart===item?{borderTop:'4px solid #ff2625',backgroundColor:'#fff',borderBottomLeftRadius:'20px'}:''}>
+    <Stack type='button' alignItems='center' justifyContent='center' className='bodyPart-card' sx={{borderTop:BodyPart===item?'4px solid #ff2625':'',backgroundColor:'#fff',borderBottomLeftRadius:'20px',width:'270px',height:'280px',cursor:'pointer',gap:'47px'}}>
         <img src={Icon} alt="dumble" style={{width:'40px',height:'40px'}}/>
+        <Typography fontSize='24px' fontWeight='bold' color='#3A1212' textTransform='uppercase'>{item}</Typography>
     </Stack>
   )
 }
--- src/Components/HorizontalScrollbar.jsx
+++ src/Components/HorizontalScrollbar.jsx
@@ -1,16 +1,40 @@
 import { Box } from '@mui/system'
-import React from 'react'
+import React, { useContext } from 'react'
+import { ScrollMenu, VisibilityContext } from 'react-horizontal-scrolling-menu'
 import BodyPart from './BodyPart'
+import RightArrowIcon from '../assets/icons/right-arrow.png';
+import LeftArrowIcon from '../assets/icons/left-arrow.png';
+import { Typography } from '@mui/material';
+
+const LeftArrow = () => {
+  const { scrollPrev } = useContext(VisibilityContext);
+
+  return (
+    <Typography onClick={() => scrollPrev()} className="right-arrow">
+      <img src={LeftArrowIcon} alt="right-arrow" />
+    </Typography>
+  );
+};
+
+const RightArrow = () => {
+  const { scrollNext } = useContext(VisibilityContext);
+
+  return (
+    <Typography onClick={() => scrollNext()} className="left-arrow">
+      <img src={RightArrowIcon} alt="right-arrow" />
+    </Typography>
+  );
+};
 
 const HorizontalScrollbar = ({data,bodyPart,setBodyPart}) => {
   return (
-    <div>
+    <ScrollMenu LeftArrow={LeftArrow} RightArrow={RightArrow}>
         {data.map((item)=>(
             <Box key={item.id||item} itemId={item.id||item} title={item.id||item} m='0 40px'>
                 <BodyPart item={item} bodyPart={bodyPart} setBodyPart={setBodyPart}/>
             </Box>
         ))}
-    </div>
+    </ScrollMenu>
   )
 }
 
