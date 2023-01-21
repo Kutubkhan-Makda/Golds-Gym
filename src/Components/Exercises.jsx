db01b09d6edf30758a82dd796a1a8c69d25c4733
MKhan1792 <kutubkhanmakda1792@gmail.com>
2023-01-22 00:00:52 +0530

    day-11

--- src/Components/Exercises.jsx
+++ src/Components/Exercises.jsx
@@ -1,10 +1,12 @@
-import { Box } from '@mui/material'
+import { Box, Typography } from '@mui/material'
 import React from 'react'
 
 const Exercises = ({exercises,setExercises,bodyPart}) => {
   return (
-    <Box id='exercises' sx={{mt:{lg:'110px'}}}>
-
+    <Box id='exercises' sx={{mt:{lg:'110px'}}} mt='50px' p='20px'>
+      <Typography variant='h3' md='46px'>
+        Showing Results
+      </Typography>
     </Box>
   )
 }
