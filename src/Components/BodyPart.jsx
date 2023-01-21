cfb3d4cc4cd8de7dc3dab2942c4327d46b9363ca
MKhan1792 <kutubkhanmakda1792@gmail.com>
2023-01-21 20:11:50 +0530

    day-11

--- src/Components/BodyPart.jsx
+++ src/Components/BodyPart.jsx
@@ -4,7 +4,7 @@ import Icon from '../assets/icons/gym.png'
 
 const BodyPart = ({item,setBodyPart,BodyPart}) => {
   return (
-    <Stack type='button' alignItems='center' justifyContent='center' className='bodyPart-card'>
+    <Stack type='button' alignItems='center' justifyContent='center' className='bodyPart-card' sx={BodyPart===item?{borderTop:'4px solid #ff2625',backgroundColor:'#fff',borderBottomLeftRadius:'20px'}:''}>
         <img src={Icon} alt="dumble" style={{width:'40px',height:'40px'}}/>
     </Stack>
   )
