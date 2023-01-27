572424461de7b46b945b7bc74fd3abc40b1f25a5
MKhan1792 <kutubkhanmakda1792@gmail.com>
2023-01-27 12:41:38 +0530

    day-14

--- src/Components/ExerciseCard.jsx
+++ src/Components/ExerciseCard.jsx
@@ -1,8 +1,18 @@
-import React from 'react'
+import { Button } from '@mui/material'
+import { Stack } from '@mui/system'
+import React, { lazy } from 'react'
+import { Link } from 'react-router-dom'
 
-const ExerciseCard = () => {
+const ExerciseCard = ({exercise}) => {
   return (
-    <div>ExerciseCard</div>
+    <Link className='exercise-card' to={`/exercise/${exercise.id}`}>
+      <img src={exercise.gifUrl} alt={exercise.name} loading={lazy}/>
+      <Stack direction='row'>
+        <Button sx={{ml:'21px',color:'#fff',background:'#ffa9a9',fontSize:'14px',borderRadius:'20px',textTransform:'capitalize'}}>
+          {exercise.bodyPart}
+        </Button>
+      </Stack>
+    </Link>
   )
 }
 
