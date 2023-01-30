53fd09c7bc35de188ac64c0b79fc745c237bbee8
MKhan1792 <kutubkhanmakda1792@gmail.com>
2023-01-30 16:12:57 +0530

    day-16

--- src/Components/ExerciseCard.jsx
+++ src/Components/ExerciseCard.jsx
@@ -1,4 +1,4 @@
-import { Button, Pagination, Typography } from '@mui/material'
+import { Button, Typography } from '@mui/material'
 import { Stack } from '@mui/system'
 import React, { lazy } from 'react'
 import { Link } from 'react-router-dom'
@@ -17,11 +17,6 @@ const ExerciseCard = ({exercise}) => {
         <Typography ml='21px' color='#000' fontWeight='bold' mt='11px' pd='10px' textTransform='capitalize' fontSize='22px'>
           {exercise.name}
         </Typography>
-      </Stack>
-      <Stack mt='100px' alignItems='center'>
-        {exercise.length>9 && (
-          <Pagination/>
-        )}
       </Stack>
     </Link>
   )
